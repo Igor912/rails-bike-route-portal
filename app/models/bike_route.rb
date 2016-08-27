d8fbbca559f639bfba8032eca7a053667d5a427f
Ihor Khomiak <igor.homyak@gmail.com>
2016-08-28 00:36:18 +0300

    added validation and fixed styles of validation messages

--- app/models/bike_route.rb
+++ app/models/bike_route.rb
@@ -1,3 +1,6 @@
 class BikeRoute < ApplicationRecord
   belongs_to :user
+
+  validates :title, :description, :user_id, presence: { message: "не може бути пустим" }
+  validates :description, length: { minimum: 8, message: " занадто короткий" }
 end
