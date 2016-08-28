15437bc102cd25ff29ca3ed19b33a1955fbf7111
Ihor Khomiak <igor.homyak@gmail.com>
2016-08-28 18:39:40 +0300

    added images to bike_route

--- app/models/bike_route.rb
+++ app/models/bike_route.rb
@@ -1,6 +1,9 @@
 class BikeRoute < ApplicationRecord
   belongs_to :user
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   validates :title, :description, :user_id, presence: { message: "не може бути пустим" }
   validates :description, length: { minimum: 8, message: " занадто короткий" }
 end
