971b21d5471f36faff4c372a563f9d4868fa8e21
Ihor Khomiak <igor.homyak@gmail.com>
2016-08-31 23:21:00 +0300

    implemented ajax and jquery effects to comments component

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,10 @@ class CommentsController < ApplicationController
     @bike_route = BikeRoute.find(params[:bike_route_id])
     @comment = @bike_route.comments.create(params[:comment].permit(:name, :body))
 
-    redirect_to bike_route_path(@bike_route)
+    respond_to do |format|
+      format.html {redirect_to bike_route_path(@bike_route)}
+      format.js
+    end
   end
 
   def destroy
